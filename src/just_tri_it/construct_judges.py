0400e53651428a20d4f9f75fcbdbdef3eaf748b4
Sergey Mechtaev <mechtaev@gmail.com>
2025-09-23 09:35:07 +0800

    script for testing judges

--- src/just_tri_it/construct_judges.py
+++ src/just_tri_it/construct_judges.py
@@ -5,7 +5,7 @@ import json
 from just_tri_it.logic import check, Side
 from just_tri_it.executor import Executor
 from just_tri_it.program import Program
-from just_tri_it.triangulation import make_partial_for_inv
+from just_tri_it.triangulation import make_postcondition
 
 
 def parse_args():
@@ -16,19 +16,19 @@ def parse_args():
         help="Set virtual environment for testing generated programs."
     )
     parser.add_argument(
-        "--left",
+        "--program",
         type=str,
         required=True,
         help="Left program."
     )
     parser.add_argument(
-        "--right",
+        "--judge",
         type=str,
         required=True,
         help="Right program."
     )
     parser.add_argument(
-        "--left-inputs",
+        "--inputs",
         type=str,
         required=True,
         help="Inputs."
@@ -39,19 +39,19 @@ def main():
     args = parse_args()
 
     executor = Executor(Path(args.test_venv))    
-    left_program_file = Path(args.left)
-    right_program_file = Path(args.right)
-    left_inputs_file = Path(args.left_inputs)
+    left_program_file = Path(args.program)
+    right_program_file = Path(args.judge)
+    inputs_file = Path(args.inputs)
 
     left_program = Program.from_function_code(left_program_file.read_text(encoding="utf-8"))
     right_program = Program.from_function_code(right_program_file.read_text(encoding="utf-8"))
-    left_inputs = json.loads(left_inputs_file.read_text(encoding="utf-8"))
+    inputs = json.loads(inputs_file.read_text(encoding="utf-8"))
         
 
-    tri = make_partial_for_inv(2, 0)
+    tri = make_postcondition(len(left_program.signature.params))
 
     check(executor,
-          { Side.LEFT: left_inputs, Side.RIGHT: None },
+          { Side.LEFT: inputs, Side.RIGHT: None },
           { Side.LEFT: left_program, Side.RIGHT: right_program },
           tri.hyperproperty)
 
