e1f100ea78bcf1fd75904b2469bb055f79fff426
“yihandai3826” <yihandai0626@126.com>
2025-11-08 01:54:44 +0000

    for some problems

--- src/just_tri_it/program.py
+++ src/just_tri_it/program.py
@@ -214,7 +214,7 @@ class Program(ContentAddressable):
         """
         outcomes = []
         never_fails = True
-        if hack(task = "atcoder_abc388_c") or hack(task = "atcoder_abc391_f") or hack(task = "atcoder_abc397_f"):
+        if hack(task = ["atcoder_abc388_c", "atcoder_abc391_f", "atcoder_abc397_f", "leetcode_3722", "leetcode_3720", "leetcode_3714"]):
             timeout_num = 0
             from just_tri_it.executor import Timeout
             for test in tests:
