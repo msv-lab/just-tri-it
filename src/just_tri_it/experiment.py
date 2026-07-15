a80d65a29194efaa296a0da2e0f34dfa03f4e209
yihandai3826 <yihandai0626@126.com>
2026-07-15 18:32:44 +0800

    temp

--- src/just_tri_it/experiment.py
+++ src/just_tri_it/experiment.py
@@ -177,13 +177,13 @@ def main():
         just_tri_it.config.NUM_RIGHT_SAMPLES = args.num_right_samples
 
     model = {
-        "gpt-4o": Yunwu("gpt-4o", 1.0, max_batch=just_tri_it.config.NUM_LEFT_SAMPLES),
-        "deepseek-v3": XMCP("ali/deepseek-v3", 1.0, alias="deepseek-v3"),
-        "gemini-2.5-flash": Yunwu("gemini-2.5-flash", 1.0)
+        "gpt-4o": Yunwu("gpt-4o", 0.5, max_batch=just_tri_it.config.NUM_LEFT_SAMPLES),
+        "deepseek-v3": XMCP("ali/deepseek-v3", 0.5, alias="deepseek-v3"),
+        "gemini-2.5-flash": Yunwu("gemini-2.5-flash", 0.5)
     }[args.model]
     just_tri_it.utils.CURRENT_MODEL = args.model
     
-    # model = XMCP(args.model, 1.0)
+    # model = XMCP(args.model, 0.5)
 
     model = setup_cache(model, args)
 
